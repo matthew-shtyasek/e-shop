deb53e015407ff8d4142d1f355bf9535553b2b9f
matthew <email@email.ru>
2023-03-11 13:08:14 +0500

    добавили переход по ссылке из списка товаров

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ from catalog.views import product_details_view, product_list_view
 app_name = 'catalog'
 
 urlpatterns = [
-    path('<int:pk>/', product_details_view, name='detail'),
+    path('<int:product_id>/', product_details_view, name='detail'),
     path('list/', product_list_view, name='list'),
 ]
