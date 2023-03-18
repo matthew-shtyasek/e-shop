06938d8e5fdbdd63209b5f397acb6767a8704b2f
matthew <email@email.ru>
2023-03-18 10:15:29 +0500

    добавили работу через class Cart в views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 
+from .cart import Cart
 from .forms import Product2CartForm
 from .models import Product
 
 
 def product_details_view(request, product_id):
     prod = get_object_or_404(Product, id=product_id)
+    cart = Cart(request)
 
     if request.method == 'POST':
         form = Product2CartForm(request.POST)
         if form.is_valid():
-            count = form.cleaned_data['count']
-            if 'cart' in request.session.keys():
-                request.session['cart'][product_id] = count
-            else:
-                request.session['cart'] = {product_id: count}
+            # count = form.cleaned_data['count']
+            cart[product_id] = form.cleaned_data['count']
             form = Product2CartForm()
     else:
         form = Product2CartForm()
 
     context = {'product': prod,  # product = prod
-               'form': form}
+               'form': form,
+               'in_cart': product_id in cart.session['cart'].keys()}
     return render(request, 'catalog/product_details.html', context)
 
 
