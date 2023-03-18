a71b6fadeb57fb54d3aa7e1f61eef698a07c7ea9
matthew <email@email.ru>
2023-03-18 13:44:48 +0500

    вывод всех товаров в корзину

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from cart.views import CartListView
+
 app_name = 'cart'
 
 urlpatterns = [
-
+    path('', CartListView.as_view(), name='cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from catalog.models import Product
+
 
 # Create your views here.
+class CartListView(ListView):
+    model = Product
+    template_name = 'cart/cart.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
