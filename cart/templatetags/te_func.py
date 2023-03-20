0f4ee7e3173315084b6644b9503099e4f3d06f43
matthew <email@email.ru>
2023-03-20 10:45:49 +0500

    подключили jquery

--- cart/templatetags/te_func.py
+++ cart/templatetags/te_func.py
@@ -9,4 +9,10 @@ def get_item(d: dict, key):
     return d[str(key)]
 
 
-#  pk -> count
+@register.filter
+def prod_count(d: dict):
+    return sum(map(int, list(d.values())))
+# получаем из словаря значения (values)
+# преобразовываем их в список (list)
+# затем в int (map)
+# и суммируем (sum)
--- e_shop/settings.py
+++ e_shop/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-#WSGI_APPLICATION = 'e_shop.wsgi.application'
+WSGI_APPLICATION = 'e_shop.wsgi.application'
 
 
 # Database
@@ -123,4 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
