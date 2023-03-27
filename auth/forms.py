2bc9a70f8c15a0f40b9300f976b1290c51a55b82
matthew <email@email.ru>
2023-03-27 12:43:23 +0500

    создали модельную форму регистрации

---
+++ auth/forms.py
@@ -0,0 +1,39 @@
+from django import forms as dj_forms
+from django.contrib.auth import forms
+
+from auth.models import CustomUser
+
+
+class CustomUserRegister(forms.UserCreationForm):
+    username = dj_forms.CharField(max_length=32,
+                                  min_length=6,
+                                  label='Логин')
+    email = dj_forms.EmailField(max_length=128,
+                                min_length=8,
+                                label='Электронная почта')
+    first_name = dj_forms.CharField(max_length=32,
+                                    min_length=4,
+                                    label='Имя')
+    last_name = dj_forms.CharField(max_length=32,
+                                   min_length=4,
+                                   label='Фамилия')
+    password1 = dj_forms.CharField(widget=dj_forms.PasswordInput,
+                                   label='Пароль')
+    password2 = dj_forms.CharField(widget=dj_forms.PasswordInput,
+                                   label='Повторите пароль')
+
+    class Meta:
+        model = CustomUser
+        fields = ('username',
+                  'first_name',
+                  'last_name',
+                  'email',
+                  'password1',
+                  'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, item in self.fields.items():
+            item.widget.attr['class'] = 'form-control'
+            item.help_text = ''
