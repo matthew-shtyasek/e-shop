fe46a85f0df90363b2db0eee29a134c7aec46909
matthew <email@email.ru>
2023-03-27 13:10:11 +0500

    создали обработчик RegisterView

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from auth.forms import CustomUserRegister
+from auth.models import CustomUser
+
+
+class RegisterView(CreateView):
+    model = CustomUser
+    form_class = CustomUserRegister
+    template_name = 'auth/register.html'
+    success_url = reverse_lazy('cat:list')
+
+    def post(self, request, *args, **kwargs):
+        result = super().post(request, *args, **kwargs)
+        return result
