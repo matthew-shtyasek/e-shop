e3851a0fcb74fee4f9116c3e4a9497b4c8c9ab94
matthew <email@email.ru>
2023-03-27 13:30:52 +0500

    добавили настройки и функцию для отправки сообщений на email

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -14,4 +16,11 @@ class RegisterView(CreateView):
 
     def post(self, request, *args, **kwargs):
         result = super().post(request, *args, **kwargs)
+        '''from django.core.mail import send_mail'''
+        '''from django.conf import settings'''
+        message = render(request, 'auth/vefiry_register.html')
+        send_mail(subject='Регистрация на сайте лучшего интернет-магазина',
+                  html_message=message,
+                  from_email=settings.EMAIL_HOST_USER,
+                  recipient_list=[request.POST.get('email')])
         return result
--- e_shop/settings.py
+++ e_shop/settings.py
@@ -133,3 +133,10 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'auth.backends.EmailAuthBackend',
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mettewjowannini@gmail.com'
+EMAIL_HOST_PASSWORD = 'parolya-net'
