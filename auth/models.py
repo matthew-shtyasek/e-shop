59f5de63eb0eba92dfe00d64114ab85f2c59e38c
matthew <email@email.ru>
2023-03-27 10:09:33 +0500

    создали модель CustomUser и  начали создавать object manager CustomUserManager

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
 from django.db import models
 
-# Create your models here.
+
+# CustomUser.objects
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, *args, **kwargs):  # для создания пользователя
+        if not email or not password:
+            raise ValueError('Email или password являются None!')
+        if 'username' not in kwargs.keys():
+            raise ValueError('Username должен быть задан!')
+
+        user = CustomUser(email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    email = models.EmailField(max_length=128,
+                              unique=True,
+                              db_index=True,
+                              blank=False)
+    is_active = models.BooleanField(blank=False,
+                                    default=False)
+    last_entry_date = models.DateTimeField(auto_now=True)
